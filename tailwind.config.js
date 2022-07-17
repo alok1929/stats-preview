d41d2201c14e5da29d600cc084b3d424a5fdcac0
alok1929 <alokhegde221@gmail.com>
2022-07-18 02:52:52 +0530

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@
 module.exports = {
   content: ["/src/index.html", "./src/**/*.{html,js}"],
   theme: {
-    colors:{
-     Verydarkblue: "hsl(233, 47%, 7%)",
-    Darkdesaturatedblue : "hsl(244, 38%, 16%)",
-    Softviolet : "hsl(277, 64%, 61%)",
-    White : "hsl(0, 0%, 100%)",
-    Slightlytransparentwhite: "hsla(0, 0%, 100%, 0.75)",
-    SlightlytransparentwhiE : "hsla(0, 0%, 100%, 0.6)",
+    colors: {
+      Verydarkblue: "hsl(233, 47%, 7%)",
+      Darkdesaturatedblue: "hsl(244, 38%, 16%)",
+      Softviolet: "hsl(277, 64%, 61%)",
+      White: "hsl(0, 0%, 100%)",
+      Slightlytransparentwhite: "hsla(0, 0%, 100%, 0.75)",
+      SlightlytransparentwhiE: "hsla(0, 0%, 100%, 0.6)",
     },
 
     extend: {},
   },
   plugins: [],
-}
+};
